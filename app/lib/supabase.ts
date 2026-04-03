a4470e2d63ee052d01e8ba0c34f2afa0617cc491
SH kim <shkim@SHui-iMac.local>
2026-04-03 23:42:16 +0900

    fix supabase client

--- app/lib/supabase.ts
+++ app/lib/supabase.ts
@@ -1,6 +1,8 @@
-import { createClient } from '@supabase/supabase-js';
+import { createBrowserClient } from "@supabase/ssr";
 
-const supabaseUrl = 'https://kjhxxncywdvprxgwscml.supabase.co';
-const supabaseKey = 'sb_publishable_kSGWkvAv_BuTYe5VhfBCjw_gvLeZGuG';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export function createClient() {
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
+}
